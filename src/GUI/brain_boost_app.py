0a76a3d14f5dbe4b671494c124fddda669cfc2ba
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-11-11 22:18:27 +0100

    initialization GUI app

---
+++ src/GUI/brain_boost_app.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from src.GUI.authorization import MenuScreen, LoginScreen
+
+class NavigationManager(ScreenManager):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Adding screen to the manager
+        self.add_widget(LoginScreen(name="login"))
+        self.add_widget(MenuScreen(name="menu"))
+
+    def go_to_screen(self, screen_name: str) -> None:
+        self.current = screen_name
+
+class BrainBoostApp(App):
+    def build(self):
+        return NavigationManager()
+
+if __name__ == "__main__":
+    BrainBoostApp().run()
