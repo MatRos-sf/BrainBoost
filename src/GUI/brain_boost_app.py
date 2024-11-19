a7e0f1f1d5aa74718d27bcb5d456a01ecf27477c
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-11-19 20:58:54 +0100

    add db to the app

--- src/GUI/brain_boost_app.py
+++ src/GUI/brain_boost_app.py
@@ -1,11 +1,18 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 
+from ..db.db import DATABASE_URL, engine, session
 from .authorization import LoginScreen
 from .menu import MenuScreen
 
 
 class MyApp(App):
+    def __init__(self, database_url=DATABASE_URL, **kwargs):
+        super(MyApp, self).__init__(**kwargs)
+        # Initialize database
+        self.db_engine = engine(database_url)
+        self.session = session(self.db_engine)
+
     def build(self):
         sm = ScreenManager()
         sm.add_widget(LoginScreen(name="login"))
