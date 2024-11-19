1a8af509a2d6c178fa91285dd9a6ab0432b12ef6
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-11-19 20:50:00 +0100

    Add database module with SQLModel integration

---
+++ src/db/db.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from ..models import ModelBase
+
+DATABASE_URL = "sqlite:///db.sqlite"
+
+
+def engine(database_url=DATABASE_URL):
+    engine = create_engine(database_url, echo=True)
+    ModelBase.metadata.create_all(engine)
+    return engine
+
+
+def session(engine):
+    return Session(engine)
