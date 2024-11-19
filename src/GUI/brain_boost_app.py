b6f416693046bb14f5e5c143aa56a1335295ea6f
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-11-19 21:53:52 +0100

    add session to the main app

--- src/GUI/brain_boost_app.py
+++ src/GUI/brain_boost_app.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 
 from ..db.db import DATABASE_URL, engine, session
-from .authorization import LoginScreen
+from .authorization import CreateAccountScreen, LoginScreen
 from .menu import MenuScreen
 
 
@@ -11,14 +11,22 @@ class MyApp(App):
         super(MyApp, self).__init__(**kwargs)
         # Initialize database
         self.db_engine = engine(database_url)
-        self.session = session(self.db_engine)
+        self.db_session = session(self.db_engine)
 
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(LoginScreen(name="login"))
+        sm.add_widget(LoginScreen(session=self.db_session, name="login"))
         sm.add_widget(MenuScreen(name="menu"))
+        sm.add_widget(
+            CreateAccountScreen(session=self.db_session, name="create_account")
+        )
         sm.current = "login"
         return sm
+
+    def on_stop(self):
+        # Clean up database session when app closes
+        if hasattr(self, "db_session"):
+            self.db_session.close()
 
 
 if __name__ == "__main__":
