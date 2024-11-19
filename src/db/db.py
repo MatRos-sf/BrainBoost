c16642039690d0faeaa40ca2be8b617442332c57
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-11-19 21:52:42 +0100

    add typing and change session function

--- src/db/db.py
+++ src/db/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import sessionmaker
 
 from ..models import ModelBase
@@ -6,12 +6,12 @@ from ..models import ModelBase
 DATABASE_URL = "sqlite:///db.sqlite"
 
 
-def engine(database_url=DATABASE_URL):
+def engine(database_url: str = DATABASE_URL) -> Engine:
     engine = create_engine(database_url, echo=True)
     ModelBase.metadata.create_all(engine)
     return engine
 
 
-def session(engine):
+def session(engine: Engine) -> sessionmaker:
     Session = sessionmaker(bind=engine)
     return Session()
