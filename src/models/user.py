8ffb2df4d1e8af77eba0c596270d6f8d1a181402
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-11-20 21:07:16 +0100

    fix(datetime) issue

--- src/models/user.py
+++ src/models/user.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 from typing import List, Optional
 
 from sqlmodel import Field, Relationship
@@ -12,7 +12,7 @@ class User(ModelBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(unique=True)
     password: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
     logins: List["Login"] = Relationship(back_populates="user")
 
 
@@ -21,5 +21,5 @@ class Login(ModelBase, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user_table.id")
-    login_date: datetime = Field(default_factory=datetime.utcnow)
+    login_date: datetime.datetime = Field(default_factory=datetime.datetime.now)
     user: User = Relationship(back_populates="logins")
