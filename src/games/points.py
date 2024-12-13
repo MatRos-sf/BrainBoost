8651a915ba292f1761d352726180a668b23c0a59
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-12-13 23:25:18 +0100

    update ResultKeeper:
    * add new fields: level_start, steps, finish_round, range_min
    * create new method get_stats
    * fix bug with level up
    
    Update class Points:
    
    * add new attribute: correct_answers, wrong_answer

--- src/games/points.py
+++ src/games/points.py
@@ -6,9 +6,13 @@ class Points:
         self.level = level
         self.points = 0
         self.answers_status: List[bool] = []
+        self.correct_answers: int = 0
+        self.wrong_answer: int = 0
 
     def update_points(self, is_wrong_answer: bool = False, bonus: int = 1) -> None:
         if is_wrong_answer:
             self.points -= self.level * bonus
+            self.wrong_answer += 1
         else:
             self.points += self.level * bonus
+            self.correct_answers += 1
