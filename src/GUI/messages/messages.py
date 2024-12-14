870d57d23d4506f928212bc3360ec81b1256a793
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-12-14 12:47:18 +0100

    create message directory - here should be all Message that will be popup in gui app

---
+++ src/GUI/messages/messages.py
@@ -0,0 +1,29 @@
+__all__ = ["message_ends_game"]
+from typing import Optional
+
+MESSAGE_TIMES_UP = "Game Over - Time's up!\n"
+MESSAGE_DEFEATED = "Game Over - You have been defeated.\n"
+
+
+def stats_message(points: int, level: int) -> str:
+    return f"You earned {points} points at level {level}!\n"
+
+
+def message_ends_game(
+    points: int,
+    level: int,
+    is_time_over: bool = False,
+    additionally_message: Optional[str] = None,
+) -> str:
+    """
+    Function crete message depends on the result games if time is over user get message: 'Game Over - Time's up!' otherwise
+    message is: 'Game Over - You have been defeated'.
+    In addition, at the very end, information about statistics and an optional message is added.
+    """
+    message = ""
+    if is_time_over:
+        message += MESSAGE_TIMES_UP
+    else:
+        message += MESSAGE_DEFEATED
+    message += stats_message(points, level)
+    return message + additionally_message if additionally_message else message
