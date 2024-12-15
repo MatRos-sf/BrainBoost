cd6983483167628f39d8d7abfb1baa4990360a98
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-12-15 13:08:20 +0100

    * create new model PointsModel which keep information about assigned points * create 2 events: add_first_points_after_create_account, update_point_in_user_model * update User model: rename points field to point add relationship with PointsModel

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,9 @@
 import datetime
+from enum import Enum
 from typing import List, Optional
 
+from sqlalchemy import event
+from sqlalchemy.orm import Session
 from sqlmodel import Field, Relationship
 
 from . import ModelBase
@@ -17,9 +20,12 @@ class User(ModelBase, table=True):
     logins: List["Login"] = Relationship(back_populates="user")
 
     # stats
-    points: int = Field(default=0)
+    point: int = Field(default=0)  # sum points
+    points: List["PointsModel"] = Relationship(back_populates="user")
     # games one-to-one relationship
     result_keeper: Optional["ResultKeeperModel"] = Relationship(back_populates="user")
+
+    # TODO: scal day -> [PN,NDZ]
 
 
 class Login(ModelBase, table=True):
@@ -29,3 +35,61 @@ class Login(ModelBase, table=True):
     user_id: int = Field(foreign_key="user_table.id")
     login_date: datetime.datetime = Field(default_factory=datetime.datetime.now)
     user: User = Relationship(back_populates="logins")
+
+
+class PointsCategory(Enum):
+    """
+    This class return tuple where:
+        first element is string. It uses to assign in Points.category models
+        second element is integer. It uses to give user points for activity.
+    When second element is -1 it means: points are dynamically assigned
+
+    """
+
+    # ACTIVITY
+    CREATE_ACCOUNT = "Create Account", 10
+    DAILY_LOGIN = "Daily", 5
+    WEEKLY_LOGIN = "Weekly", 10
+    # Games
+    GAME_RESULT_KEEPER = "Game Result Keeper", -1
+    FIRST_RESULT_KEEPER = "First Game Result Keeper", 10
+
+
+class PointsModel(ModelBase, table=True):
+    """Models hold information about assigned points"""
+
+    __tablename__ = "points_table"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user_table.id")
+    user: User = Relationship(back_populates="points")
+    category: str
+    point: int
+    saved_date: datetime.datetime = Field(default_factory=datetime.datetime.now)
+
+
+# Events
+
+
+@event.listens_for(User, "after_insert")
+def add_first_points_after_create_account(mapper, connection, target):
+    """When user create account should get points for it"""
+    with Session(bind=connection) as session:
+        session.add(
+            PointsModel(
+                user_id=target.id,
+                category=PointsCategory.CREATE_ACCOUNT.value[0],
+                point=PointsCategory.CREATE_ACCOUNT.value[1],
+            )
+        )
+        session.commit()
+
+
+@event.listens_for(PointsModel, "after_insert")
+def update_point_in_user_model(mapper, connection, target):
+    """Events update point in User model when PointsModel is added"""
+    connection.execute(
+        User.__table__.update()
+        .where(User.id == target.user_id)
+        .values(point=User.__table__.c.point + target.point)
+    )
