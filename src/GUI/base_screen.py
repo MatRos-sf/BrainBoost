95a2f2efea493acbd435c50aa2260d2e319a8eb1
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-01-12 19:05:27 +0100

    create method get_label_with_variables

--- src/GUI/base_screen.py
+++ src/GUI/base_screen.py
@@ -23,18 +23,74 @@ class BaseScreen(Screen):
             )
 
     def set_label_text(self, **kwargs):
+        print("HEHEHEHEHEHEHEHHEHEHEHEHE")
+        print(kwargs)
         for key, value in kwargs.items():
             attr = getattr(self, key)
             setattr(attr, "text", value)
 
-    def get_message_with_variables(self, name_screen: str, key: str, **variables):
+    def format_text(self, text: str, name_screen: str, key: str, **variables) -> str:
         """
-        Format the message using the provided variables
+        Format the given text by replacing placeholders with provided variables.
+
+        This function takes a text string and formats it using the provided variables.
+        If the text is None, it raises a ValueError with information about the missing message.
+
+        Args:
+            text (str): The text to be formatted.
+            name_screen (str): The name of the screen associated with the text.
+            key (str): The key identifying the specific text within the screen.
+            **variables: Arbitrary keyword arguments representing variables to be inserted into the text.
+
+        Returns:
+            str: The formatted text with variables inserted.
+
+        Raises:
+            ValueError: If the input text is None, indicating a missing message implementation.
         """
-        text = self.translation.get_messages_text(name_screen, key)
         if text is None:
             raise ValueError(f"Please implement massage for: {name_screen=}, {key=}")
         return text.format(**variables)
+
+    def get_label_with_variables(self, name_screen: str, key: str, **variables) -> str:
+        """
+        Retrieve and format a label text with variables.
+
+        This function gets a label text for a specific screen and key, then formats it
+        with the provided variables.
+
+        Args:
+            name_screen (str): The name of the screen associated with the label.
+            key (str): The key identifying the specific label within the screen.
+            **variables: Arbitrary keyword arguments representing variables to be
+                         inserted into the label text.
+
+        Returns:
+            str: The formatted label text with variables inserted.
+        """
+        text = self.translation.get_labels_text(name_screen, key)
+        return self.format_text(text, name_screen, key, **variables)
+
+    def get_message_with_variables(
+        self, name_screen: str, key: str, **variables
+    ) -> str:
+        """
+        Retrieve and format a message text with variables.
+
+        This function gets a message text for a specific screen and key, then formats it
+        with the provided variables.
+
+        Args:
+            name_screen (str): The name of the screen associated with the message.
+            key (str): The key identifying the specific message within the screen.
+            **variables: Arbitrary keyword arguments representing variables to be
+                         inserted into the message text.
+
+        Returns:
+            str: The formatted message text with variables inserted.
+        """
+        text = self.translation.get_messages_text(name_screen, key)
+        return self.format_text(text, name_screen, key, **variables)
 
     def init_first_game(self, target_screen: str):
         """
